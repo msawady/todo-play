92dd062bccdead3673bd06852638325b9593f447
msawady <masaaki2110@gmail.com>
2017-09-04 23:48:42 +0900

    sql追加

---
+++ conf/evolutions/1.sql
@@ -0,0 +1,13 @@
+# Todo schema
+
+# --- !Ups
+
+create table todo(id bigint(20) NOT NULL AUTO_INCREMENT, status varchar(31) NOT NULL,title varchar(255) NOT NULL, PRIMARY KEY (ID)) AUTO_INCREMENT=10000
+
+insert into todo(id, status, title) values(null, 'UNDONE', 'すごい機能の要件定義');
+insert into todo(id, status, title) values(null, 'UNDONE', 'すごい機能の設計');
+insert into todo(id, status, title) values(null, 'UNDONE', 'すごい機能の実装');
+
+# --- !Downs
+
+DROP TABLE if exists TODO;
