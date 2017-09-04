7dc4b6f3d8f7e3bfbd772da554b9cd1a9f762232
msawady <masaaki2110@gmail.com>
2017-09-05 07:06:05 +0900

    use database at repository

--- app/services/repository/TodoRepository.scala
+++ app/services/repository/TodoRepository.scala
@@ -1,57 +1,33 @@
 package services.repository
 
-import java.io.PrintWriter
+import javax.inject.Inject
 
 import com.google.inject.Singleton
-import play.Environment
-import services.domain.{Todo, TodoStatus}
+import services.domain.{Todo, TodoStatus, Todos}
+import slick.driver.MySQLDriver.api._
 
-import scala.collection.mutable
-import scala.io.Source
+import scala.concurrent.Await
+import scala.concurrent.duration.Duration
 
 /**
   * repository class for todo management
   *
   */
 @Singleton
-class TodoRepository() {
+class TodoRepository {
 
-  private val filePath = "data/TODO.txt"
-  private val encoding = "UTF-8"
+  val database = Database.forURL("jdbc:mysql://localhost/todo_play?characterEncoding=UTF8&useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC",
+    driver = "com.mysql.jdbc.Driver", user = "user", password = "password")
 
   def getTodoList(): List[Todo] = {
-    sortById(readTodoFile())
+    val f = database.run(Todos.sortBy(_.id).result)
+    Await.result(f, Duration.Inf).toList
   }
 
   def updateTodoList(ids: List[Int], status: TodoStatus) = {
-    val modified = readTodoFile()
-    ids.foreach(id => {
-      val todo: Todo = modified.get(id).get
-      todo.update(status)
-      modified.put(todo.getId, todo)
-    })
-    writeTodoFile(sortById(modified))
+    val toBeUpdated = Todos.filter(row => row.id inSetBind ids)
+    database.run(toBeUpdated.map(_.status).update(status))
   }
 
-  private def readTodoFile(): mutable.Map[Int, Todo] = {
-    val map: mutable.Map[Int, Todo] = new mutable.HashMap[Int, Todo]
-    val s = Source.fromFile(Environment.simple().getFile(filePath), encoding)
-    val strList: List[String] = try s.getLines.toList finally s.close()
-    strList.foreach(str => {
-      val data: Array[String] = str split "\t"
-      val todo: Todo = new Todo(data(0).toInt, TodoStatus.withName(data(1)), data(2))
-      map.put(todo.getId, todo)
-    })
-    return map
-  }
-
-  private def writeTodoFile(list: List[Todo]) = {
-    val pw = new PrintWriter(filePath, encoding)
-    try list.foreach(todo => pw.write(todo.toTsvString + "\n")) finally pw.close()
-  }
-
-  private def sortById(map: mutable.Map[Int, Todo]): List[Todo] = {
-    map.values.toList.sortBy(_.getId)
-  }
 }
 
